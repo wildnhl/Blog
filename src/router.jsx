6abb6994b2b6013583130296909e7f01ee8ec112
Aliaksei Yankouski <31486300+wildnhl@users.noreply.github.com>
2024-08-25 19:34:47 +0300

    fix: delete trailing

--- src/router.jsx
+++ src/router.jsx
@@ -15,7 +15,6 @@ export const router = createBrowserRouter([
     element: <Layout />,
     basename: "/Blog",
     children: [
-      {
         path: '/',
         element: <PostsPage />,
       {
